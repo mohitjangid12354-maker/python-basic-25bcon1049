37f3c8dcedc0f725bb2c42a01a2166cda049b54f
mohit jangid <mohitjangid12354@gmail.com>
2026-09-16 21:52:10 +0530

    feat:add stuture program

---
+++ stuture.py
@@ -0,0 +1,8 @@
+student = {}
+
+student["Name"] = input("Enter Name: ")
+student["Roll No"] = int(input("Enter Roll No: "))
+
+print("\nStudent Details")
+print("Name:", student["Name"])
+print("Roll No:", student["Roll No"])
